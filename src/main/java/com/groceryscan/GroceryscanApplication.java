4925919bbc77f26093eb771ca2e30de35d1efff2
Oleh <olegx4@ukr.net>
2020-07-11 01:44:36 +0300

    Initial commit

---
+++ src/main/java/com/groceryscan/GroceryscanApplication.java
@@ -0,0 +1,13 @@
+package com.groceryscan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroceryscanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GroceryscanApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/groceryscan/GroceryscanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groceryscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GroceryscanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
